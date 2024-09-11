250b97b5d6462f3235adeddf3faeb1de1ae8b686
Topelus <143185460+Topelus@users.noreply.github.com>
2024-09-11 20:10:10 +0100

    Add files via upload

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,44 @@
+window.onscroll = function () {
+    var header = document.getElementById("navigation");
+    if (window.pageYOffset > 200) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+};
+var menu = document.getElementById("menu");
+var openBtn = document.getElementById("openMenu");
+openBtn.addEventListener('click',function(event){
+    menu.classList.toggle('active-menu');
+    openBtn.classList.toggle('closeMenu');
+})
+
+
+var mvs = document.getElementsByClassName('ourMVs');
+
+// Convertir en tableau pour utiliser forEach
+Array.from(mvs).forEach(lists => {
+    lists.addEventListener('click', function(event) {
+        lists.classList.toggle('tooglemv');
+    });
+});
+console.log(mvs);
+var backgroundContainer = document.getElementById("parralax-header");
+var images = [
+    'url("assets/images/bannpic.webp")',
+    'url("assets/images/pexels-fauxels-3183150-min.webp")',
+    'url("assets/images/reading-glasses-piles-coins-high-view-min.webp")'
+];
+
+var currentIndex = 0;
+
+function changeBackgroundImage() {
+    // Change the background image
+    backgroundContainer.style.backgroundImage = images[currentIndex];
+    // Update the index, and loop back to the start if necessary
+    currentIndex = (currentIndex + 1) % images.length;
+}
+// Initial call to set the first background image
+changeBackgroundImage();
+// Change the background every 10 seconds (10000 milliseconds)
+setInterval(changeBackgroundImage, 10000);
