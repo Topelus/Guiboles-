9fdf606e3a4f0897a2fe795a06224b6a9fd359ee
Topelus <143185460+Topelus@users.noreply.github.com>
2024-09-11 20:07:44 +0100

    Delete assets directory

--- assets/scripts/scripts.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function () {
-    var header = document.getElementById("navigation");
-    if (window.pageYOffset > 200) {
-        header.classList.add("scrolled");
-    } else {
-        header.classList.remove("scrolled");
-    }
-};
